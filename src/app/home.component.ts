122487f5e8cbaeae29f55d0e42744ae8871c1b13
stephanblaurock <stephan.blaurock@outlook.de>
2019-08-15 14:05:46 +0200

    Flyer automatisiert

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,3 +1,5 @@
+import { FlyerInfo } from './models/flyer';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  flyerinfos: FlyerInfo[];
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.reloadFlyerInfos();
   }
 
+  reloadFlyerInfos() {
+    const url = 'https://portal.lumara.de/flyer/flyer.json';
+    this.http.get<FlyerInfo[]>(url).subscribe(data => {
+      this.flyerinfos = data;
+    });
+  }
+
+  getThumbnailUrl(myurl: string, pageno: number) {
+    if (pageno === 1) {
+      return myurl.replace('.pdf', '_01.jpg');
+    } else if (pageno === 2) {
+      return myurl.replace('.pdf', '_02.jpg');
+    }
+  }
 }
